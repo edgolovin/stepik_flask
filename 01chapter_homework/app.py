986a70442fbed47eab7ea356bff025e376970a40
Evgeniy Golovin <edgolovin@yandex.ru>
2021-01-10 00:54:49 +0700

    update app.py in 01chapter_homework

--- 01chapter_homework/app.py
+++ 01chapter_homework/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import data
 
 app = Flask(__name__)
 
@@ -16,8 +17,58 @@ def render_tours(tour_id=0):
 
 
 @app.route('/')
-def render_main():
+def render_index():
     return render_template("index.html")
+
+
+def make_tour_short_description(k):
+    country = data.tours[k]['country'] + ':'
+    href = f'<a href="/data/tours/{k}">'
+    title = data.tours[k]['title']
+    price = data.tours[k]['price']
+    stars = data.tours[k]['stars'] + '*'
+    return f'<p>{country} {href}{title} {price} {stars}</a></p>\n'
+
+
+@app.route('/data/')
+def render_data():
+    raw_html = '<h1>Все туры</h1>\n\n'
+    for k in data.tours.keys():
+        raw_html += make_tour_short_description(k)
+    return raw_html
+
+
+@app.route('/data/departures/')
+@app.route('/data/departures/<dep_id>')
+def render_data_departures(dep_id=''):
+    if not dep_id:
+        return render_data()
+    elif dep_id in data.departures.keys():
+        raw_html = f'<h1>Туры по направлению {data.departures[dep_id]}</h1>\n\n'
+        for i in range(1, 17):
+            if dep_id == data.tours[i]['departure']:
+                raw_html += make_tour_short_description(i)
+        return raw_html
+    else:
+        return 'There\'s no departure from this city'
+
+
+def make_tour_full_description(tour_id):
+    tour = data.tours[tour_id]
+    raw_html = f'<h1>{tour["country"]}: {tour["title"]} {tour["price"]}:</h1>\n\n'
+    raw_html += f'<p>{tour["nights"]} ночей</p>'
+    raw_html += f'<p>Стоимость: {tour["price"]} Р</p>'
+    raw_html += f'<p>{tour["description"]}</p>'
+    return raw_html
+
+
+@app.route('/data/tours/')
+@app.route('/data/tours/<int:tour_id>')
+def render_tour(tour_id=0):
+    if 0 < tour_id < 17:
+        return make_tour_full_description(tour_id)
+    else:
+        return 'there are 16 tours'
 
 
 def main():
