357fa483ef2795de5e62509e3c947ead41a5b1a9
Evgeniy Golovin <edgolovin@yandex.ru>
2021-02-07 21:35:02 +0700

    learn: add context_processor to app

--- 02chapter/app.py
+++ 02chapter/app.py
@@ -2,11 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+menu_links = [{'title': 'Главная', 'link': 'index'},
+              {'title': 'Неглавная', 'link': 'not-main'},
+              {'title': 'Third menu item', 'link': 'thirdlink'}]
+
 
 @app.route('/')
 @app.route('/index/')
 def render_main():
     return render_template("index.html")
+
+
+@app.route('/not-main/')
+def render_secondary():
+    return render_template("not-main.html")
+
+
+@app.context_processor
+def inject_menu_dict():
+    return dict(menu_links=menu_links)
 
 
 # set up Flask to listen to the port
